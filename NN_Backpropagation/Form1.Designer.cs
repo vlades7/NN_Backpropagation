8f1e7143156e01e2bd45b650d8208f9c56aa059e
Vladislav Bykov <vlades7@mail.ru>
2020-04-09 03:20:08 +0300

    Created project

---
+++ NN_Backpropagation/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NN_Backpropagation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.InfoPanel = new System.Windows.Forms.Panel();
+            this.Rtb_Result = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ControlPanel.SuspendLayout();
+            this.InfoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.Controls.Add(this.button2);
+            this.ControlPanel.Controls.Add(this.button1);
+            this.ControlPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ControlPanel.Location = new System.Drawing.Point(525, 0);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(275, 450);
+            this.ControlPanel.TabIndex = 0;
+            // 
+            // InfoPanel
+            // 
+            this.InfoPanel.Controls.Add(this.Rtb_Result);
+            this.InfoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InfoPanel.Location = new System.Drawing.Point(0, 0);
+            this.InfoPanel.Name = "InfoPanel";
+            this.InfoPanel.Size = new System.Drawing.Size(525, 450);
+            this.InfoPanel.TabIndex = 1;
+            // 
+            // Rtb_Result
+            // 
+            this.Rtb_Result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Rtb_Result.Location = new System.Drawing.Point(0, 0);
+            this.Rtb_Result.Name = "Rtb_Result";
+            this.Rtb_Result.Size = new System.Drawing.Size(525, 450);
+            this.Rtb_Result.TabIndex = 0;
+            this.Rtb_Result.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.InfoPanel);
+            this.Controls.Add(this.ControlPanel);
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "MainForm";
+            this.Text = "Нейронная сеть - Обратное распространение ошибки";
+            this.ControlPanel.ResumeLayout(false);
+            this.InfoPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ControlPanel;
+        private System.Windows.Forms.Panel InfoPanel;
+        private System.Windows.Forms.RichTextBox Rtb_Result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
