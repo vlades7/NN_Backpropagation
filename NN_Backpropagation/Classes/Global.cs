45f4a55bde1c0661e3e09ff88aa38d7abfa510ed
Vladislav Bykov <vlades7@mail.ru>
2020-05-21 20:33:01 +0300

    Changed APGAR in Children.xlsx

--- NN_Backpropagation/Classes/Global.cs
+++ NN_Backpropagation/Classes/Global.cs
@@ -17,14 +17,14 @@ namespace NN_Backpropagation.Classes
         public const int CountRanges = 10;                          // Количество диапазонов
         public const int NumInfoCols = 1;                           // Количество информационных параметров у образа
         public const int NumMissCols = 1;                           // Количество незначимых параметров у образа стоящих спереди
-        public const int NumParamsExcel = 8;                        // Количество значимых параметров у образа из Excel
+        public const int NumParamsExcel = 9;                        // Количество значимых параметров у образа из Excel
         public const int InputSize = CountRanges + NumParamsExcel;  // Количество нейронов на входном слое
         public const int OutputSize = 6;                            // Количество нейронов на выходном слое
 
         public const bool IsHeadDeleted = true;                     // True - если в файле есть строка с заголовками
         public const bool DeleteFirstStr = true;                    // True - если не считывать первую строку файла
 
-        public static string ConfigLayers = "9 3";                  // Конфигурация нейронной сети (скрытые слои)
+        public static string ConfigLayers = "10 5 6";               // Конфигурация нейронной сети (скрытые слои)
         public static double PartTrain = 1.0;                       // Доля обучающей выборки [0, 1]
         public static int IndexActivation = 0;                      // Индекс выбранной функции активации
         public static double Alpha = 0.5;                           // Скорость обучения
